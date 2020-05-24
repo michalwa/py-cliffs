cc8939a4ab6429f776fcef8c9b7ce7a15f710e9a
Michał Wawrzynowicz <michalwa2003@gmail.com>
2020-05-24 20:33:22 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='clifford',
+    version='1.0',
+    license='MIT',
+    description='Command parser utility',
+    long_description=long_description,
+    author='michalwa',
+    author_email='michalwa2003@gmail.com',
+    url='https://github.com/michalwa/py-clifford',
+    packages=['clifford']
+)
